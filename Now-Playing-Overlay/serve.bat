58fd4d25626c95a6d9ea9d7858af91f383afd89a
fckyough <mikehyrenza@gmail.com>
2026-04-12 06:23:33 +0800

    Fix configurator and add option to serve.bat
    configurator is kinda annoying to use since there are 50 options, but the preview is dead center so u gotta sccroll to see it, also added option to have fully hidden server process

--- Now-Playing-Overlay/serve.bat
+++ Now-Playing-Overlay/serve.bat
@@ -1,3 +1,5 @@
 @echo off
 start /min "Spectrum Server" python "%~dp0spectrum-server.py"
 start /min "Overlay Server" powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0overlay-server.ps1"
+REM no minimized window, only in TM as a powershell.exe, swap for teh above powershell line
+REM "%ComSpec%" /c C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File "%~dp0overlay-server.ps1"
